d07fb5d081859ba6963f2212e2ec0d15195fe6c8
webdev-akshay <webdev.akshay@gmail.com>
2025-01-14 14:23:32 +0530

    ui changes

--- src/app/components/salary-details/salary-details.component.ts
+++ src/app/components/salary-details/salary-details.component.ts
@@ -1,26 +1,34 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators, ɵNgNoValidate } from '@angular/forms'
 import { Router } from '@angular/router';
 import { DataService } from '../../shared/data.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-salary-details',
   standalone: true,
-  imports:[FormsModule],
+  imports:[FormsModule,CommonModule,ReactiveFormsModule],
   templateUrl: './salary-details.component.html',
   styleUrl: './salary-details.component.scss'
 })
-export class SalaryDetailsComponent {
-  formData:{salary:string,pan:string,aadhaar:string}={
-    salary:'',
-    pan:'',
-    aadhaar:''
-  }
-  constructor(private router:Router, private dataService:DataService) {
+export class SalaryDetailsComponent implements OnInit {
+  form!: FormGroup;
 
-  }
-  onNext(){
-    this.dataService.setFormData(this.formData)
-    this.router.navigate(['/pre-confirmation'])
+  constructor(private fb: FormBuilder, private router:Router) {}
 
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      salary: [0, [Validators.required, Validators.min(1)]], 
+      pan: ['', Validators.required],
+      aadhaar: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.form.valid) {
+      console.log('Form submitted:', this.form.value);
+      this.router.navigate(['/pre-confirmation'])
+    } else {
+      console.log('Form is invalid');
+    }
   }
 }
